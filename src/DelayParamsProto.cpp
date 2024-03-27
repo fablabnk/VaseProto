fd559179931e574879005efff233a0f8a153a107
Graham Booth <graham.r.booth@gmail.com>
2024-03-27 17:36:18 +0100

    working delay with two parameters and CV inputs

--- src/DelayParamsProto.cpp
+++ src/DelayParamsProto.cpp
@@ -1,53 +1,92 @@
+#include <iostream>
+#include <algorithm>
 #include "plugin.hpp"
 #include "daisysp.h"
 
-#define MAX_DELAY static_cast<size_t>(48000 * 0.75f)
+#define SAMPLE_RATE 48000
+#define MAX_DELAY static_cast<size_t>(SAMPLE_RATE * 1.0f)
 
+// clamp function
+template<typename T>
+const T& clamp(const T& value, const T& low, const T& high) {
+    return (value < low) ? low : (value > high) ? high : value;
+}
 struct DelayParamsProto : Module {
-	enum ParamIds {
-		NUM_PARAMS
+	enum ParamId {
+		PARAM_ONE_PARAM,
+		PARAM_TWO_PARAM,
+		PARAMS_LEN
 	};
-	enum InputIds {
+	enum InputId {
+		PARAM_ONE_CV_INPUT,
+		PARAM_TWO_CV_INPUT,
 		AUDIO_INPUT,
-		NUM_INPUTS
+		INPUTS_LEN
 	};
-	enum OutputIds {
+	enum OutputId {
 		AUDIO_OUTPUT,
-		NUM_OUTPUTS
+		OUTPUTS_LEN
 	};
-	enum LightIds {
-		NUM_LIGHTS
+	enum LightId {
+		LIGHTS_LEN
 	};
+
 
 	DelayParamsProto() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(PARAM_ONE_PARAM, 0.f, 1.f, 1.f, "Delay Time (secs)");
+		configParam(PARAM_TWO_PARAM, 0.f, 1.f, 1.f, "Feedback");
+		configInput(PARAM_ONE_CV_INPUT, "Delay Time CV Input");
+		configInput(PARAM_TWO_CV_INPUT, "Feedback CV Input");
+		configInput(AUDIO_INPUT, "Audio In");
+		configOutput(AUDIO_OUTPUT, "Audio Out");
 	}
 
 	void process(const ProcessArgs& args) override {
 		float feedback, del_out, sig_out;
+		
+		;
+		// the two params will be
+		// 1. delay time
+		// add param input to cv input and clip between 0 and 1
+		float paramOne = params[PARAM_ONE_PARAM].getValue() + (inputs[PARAM_ONE_CV_INPUT].getVoltage() / 5.0f);
+		float clampedParamOne = clamp(paramOne, 0.f, 1.f);
+		float rangeDelayTimeSecs = maxDelayTimeSecs - minDelayTimeSecs;
+		float delayTime = (clampedParamOne * rangeDelayTimeSecs) + minDelayTimeSecs;
+		del.SetDelay(SAMPLE_RATE * delayTime);
         del_out = del.Read();
         sig_out  = del_out + inputs[AUDIO_INPUT].getVoltage();
-        feedback = (del_out * 0.75f) + inputs[AUDIO_INPUT].getVoltage();
+		// 2. feedback amount (0 - 0.75)
+		float paramTwo = params[PARAM_TWO_PARAM].getValue() + (inputs[PARAM_TWO_CV_INPUT].getVoltage() / 5.0f);
+		float clampedParamTwo = clamp(paramTwo, 0.f, 1.f);
+        feedback = (del_out * clampedParamTwo) + inputs[AUDIO_INPUT].getVoltage();
         del.Write(feedback);
 		outputs[AUDIO_OUTPUT].setVoltage(sig_out);
 	}
 	daisysp::DelayLine<float, MAX_DELAY> del;
+	float minDelayTimeSecs = 0.1, maxDelayTimeSecs = 1.0;
 };
 
 
 struct DelayParamsProtoWidget : ModuleWidget {
 	DelayParamsProtoWidget(DelayParamsProto* module) {
 		setModule(module);
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/DelayParamsProto.svg")));
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/DelayParamsProto.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, DelayParamsProto::AUDIO_INPUT));
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, DelayParamsProto::AUDIO_OUTPUT));
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(9.23, 37.3)), module, DelayParamsProto::PARAM_ONE_PARAM));
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(9.102, 61.5)), module, DelayParamsProto::PARAM_TWO_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(23.635, 37.3)), module, DelayParamsProto::PARAM_ONE_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(23.507, 61.5)), module, DelayParamsProto::PARAM_TWO_CV_INPUT));
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 83)), module, DelayParamsProto::AUDIO_INPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.7)), module, DelayParamsProto::AUDIO_OUTPUT));
 	}
 };
-
 
 Model* modelDelayParamsProto = createModel<DelayParamsProto, DelayParamsProtoWidget>("DelayParamsProto");
